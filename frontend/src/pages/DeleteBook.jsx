fc762f6f3cd79c4f9a88fc6c5d87dee66e344ab2
jinseo0904 <jins0904@sas.upenn.edu>
2024-02-28 15:19:13 -0500

    complete

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -3,11 +3,14 @@ import axios from 'axios';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const DeleteBook = () => {
   const [loading, setLoading]  = useState(false);
   const navigate = useNavigate();
   const {id} = useParams();
+
+  const {enqueueSnackbar} = useSnackbar();
 
   const handleDeleteBook = () => {
     setLoading(true);
@@ -15,11 +18,13 @@ const DeleteBook = () => {
       .delete(`http://localhost:5555/books/${id}`)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar('Book Deleted Successfully!', { variant: 'success'});
         navigate('/');
       })
       .catch((error) => {
         setLoading(false);
-        alert('An error happened. Please check console');
+        enqueueSnackbar('Error', { variant: 'error'});
+        //alert('An error happened. Please check console');
         console.log(error);
       });
   };
